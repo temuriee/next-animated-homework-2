82fb6b1e146a97194a421d07913b2ec1b3b16c18
temuriee <critstoggle@gmail.com>
2026-01-07 12:12:11 -0800

    .

---
+++ src/app/testanimation/page.jsx
@@ -0,0 +1,9 @@
+const test = () => {
+  return (
+    <div className="h-full flex items-center justify-center">
+      <div className="w-96 h-96"></div>
+    </div>
+  );
+};
+
+export default test;
