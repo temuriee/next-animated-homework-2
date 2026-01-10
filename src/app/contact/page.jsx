b377d5a489829bfe9c01a9c9e65083562cbe2fde
temuriee <critstoggle@gmail.com>
2026-01-10 14:11:35 -0800

    .

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,5 +1,17 @@
+"use client";
+import { motion } from "framer-motion";
+
 const ContactPage = () => {
-  return <div></div>;
+  return (
+    <motion.div
+      className="h-full"
+      initial={{ y: "-200vh" }}
+      animate={{ y: "0%" }}
+      transition={{ duration: 1 }}
+    >
+      Contact
+    </motion.div>
+  );
 };
 
 export default ContactPage;
